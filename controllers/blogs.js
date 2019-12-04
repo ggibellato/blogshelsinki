79ba87b22c4ee46e33c7ab2555ab60d4058ca374
Gleison Gibellato da Silva <gleison.gibellatodasilva@grassvalley.com>
2019-12-04 09:41:43 +0000

    until 4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -17,4 +17,38 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 })
 
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch(exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  try {
+    const tempBlog = new Blog(request.body)
+    const blog = {
+      author: tempBlog.author,
+      title: tempBlog.title,
+      url: tempBlog.url,
+      likes: tempBlog.likes
+    }
+    await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+      .then(updatedBlog => {
+        response.json(updatedBlog.toJSON())
+      })
+      .catch(error => {
+        console.log('er', error)
+        next(error)
+      })
+  }
+  catch(exception) {
+    console.log('e', exception)
+    next(exception)
+  }
+})
+
+
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -14,89 +14,139 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
-test('api/blogs get', async () => {
-  const response = await api.get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-  expect(response.body.length).toBe(testHelper.listWithBlogs.length)
+describe('check blog info', () => {
+  test('Blog has id', async() => {
+    const blogs = await testHelper.blogsInDb()
+    const blog = blogs[0]
+    expect(blog.id).toBeDefined()
+  })
+
+  test('default likes 0', async () => {
+    const newBlog = {
+      title: 'New blog to test',
+      author: 'GGS',
+      url: 'http://www.u.arizona.edu'
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogSaved = new Blog(response.body)
+    expect(blogSaved.likes).toBeDefined()
+    expect(blogSaved.likes).toBe(0)
+  })
 })
 
-test('Blog has id', async() => {
-  const blogs = await testHelper.blogsInDb()
-  const blog = blogs[0]
-  expect(blog.id).toBeDefined()
+describe('blogs get api tests', () => {
+  test('api/blogs get', async () => {
+    const response = await api.get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    expect(response.body.length).toBe(testHelper.listWithBlogs.length)
+  })
 })
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'New blog to test',
-    author: 'GGS',
-    url: 'http://www.u.arizona.edu',
-    likes: 0,
-  }
+describe('blogs add api tests', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'New blog to test',
+      author: 'GGS',
+      url: 'http://www.u.arizona.edu',
+      likes: 0,
+    }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  const blogsAtEnd = await testHelper.blogsInDb()
-  expect(blogsAtEnd.length).toBe(testHelper.listWithBlogs.length + 1)
+    const blogsAtEnd = await testHelper.blogsInDb()
+    expect(blogsAtEnd.length).toBe(testHelper.listWithBlogs.length + 1)
 
-  const titles = blogsAtEnd.map(n => n.title)
-  expect(titles).toContain('New blog to test')
+    const titles = blogsAtEnd.map(n => n.title)
+    expect(titles).toContain('New blog to test')
+  })
+
+  test('add blog missing title and/or url', async () => {
+    const newBlogMissingTitle = {
+      author: 'GGS',
+      url: 'http://www.u.arizona.edu'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlogMissingTitle)
+      .expect(400)
+
+    const newBlogMissingUrl = {
+      title: 'New lbog',
+      author: 'GGS'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlogMissingUrl)
+      .expect(400)
+
+    const newBlogMissingBoth = {
+      author: 'GGS'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlogMissingBoth)
+      .expect(400)
+  })
 })
 
-test('default likes 0', async () => {
-  const newBlog = {
-    title: 'New blog to test',
-    author: 'GGS',
-    url: 'http://www.u.arizona.edu'
-  }
+describe('blogs delete api tests', () => {
 
-  const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+  test('a note can be deleted', async () => {
+    const blogsAtStart = await testHelper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
-  const blogSaved = new Blog(response.body)
-  expect(blogSaved.likes).toBeDefined()
-  expect(blogSaved.likes).toBe(0)
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await testHelper.blogsInDb()
+
+    expect(blogsAtEnd.length).toBe(
+      testHelper.listWithBlogs.length - 1
+    )
+
+    const titles = blogsAtEnd.map(b => b.titles)
+
+    expect(titles).not.toContain(blogToDelete.title)
+  })
 })
 
-test('add blog missing title and/or url', async () => {
-  const newBlogMissingTitle = {
-    author: 'GGS',
-    url: 'http://www.u.arizona.edu'
-  }
+describe('blogs put/update api tests', () => {
 
-  await api
-    .post('/api/blogs')
-    .send(newBlogMissingTitle)
-    .expect(400)
+  test('a note can be updated', async () => {
+    const blogs = await testHelper.blogsInDb()
+    const blogToUpdate = blogs[0]
 
-  const newBlogMissingUrl = {
-    title: 'New lbog',
-    author: 'GGS'
-  }
+    blogToUpdate.author = 'new author'
+    blogToUpdate.likes = 10
 
-  await api
-    .post('/api/blogs')
-    .send(newBlogMissingUrl)
-    .expect(400)
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(blogToUpdate)
+      .expect(200)
 
-  const newBlogMissingBoth = {
-    author: 'GGS'
-  }
+    const newBlogs = await testHelper.blogsInDb()
 
-  await api
-    .post('/api/blogs')
-    .send(newBlogMissingBoth)
-    .expect(400)
+    const updateBlog = newBlogs.find(b => b.id === blogToUpdate.id)
+
+    expect(updateBlog.author).toBe(blogToUpdate.author)
+    expect(updateBlog.likes).toBe(blogToUpdate.likes)
+  })
 })
-
 
 afterAll(() => {
   mongoose.connection.close()
